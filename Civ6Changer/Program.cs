e3b9070625f7cb2471817afd0a485995e6ac1de8
Emmanuel Cordero <ecordero8192@live.com>
2018-01-19 22:28:16 -0500

    Add project files.

---
+++ Civ6Changer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Civ6Changer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
